486ca2588a8720c99368afb500ab42035e0369b2
mayanja <mayanjer3@gmail.com>
2026-08-14 20:09:25 +0300

    added a create object in the payments table while the the default for payment status is PENDING

--- payments/views.py
+++ payments/views.py
@@ -6,6 +6,8 @@ from rest_framework.exceptions import NotFound
 from .models import *
 from .serializers import *
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+import json
 
 # Create your views here.
 
@@ -37,13 +39,20 @@ class PaymentViewSet(ModelViewSet):
     try:
         queryset = Payment.objects.all()
     except Payment.DoesNotExist:
-        raise NotFound({"success": False,
-                         "error_code": "payment not found",
-                         })
+        raise NotFound({"error_code": "payment not found"})
     
     def get_serializer_class(self):
         if self.action == "retrieve":
             return PaymentSerializerRetrieve
         return PaymentSerializer
     
-    
+    def create(self, request, *args, **kwargs):
+        payload = request.data.copy() #the reson we use copy is because by default data from forms is parsed to a Querydict which is immutable. It only allows mutation with data from JSON objects becuase the data is parsed to normal python dictionaries
+        if payload.get("payment_status") == None:
+            payload["payment_status"] = "PENDING"
+        serializer = self.get_serializer(data = payload)
+        serializer.is_valid(raise_exception = True)
+        self.perform_create(serializer)
+        
+        headers = self.get_success_headers(serializer.data)
+        return super().create(request, *args, **kwargs)
